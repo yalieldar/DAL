1a1929c68df014b80f814f3b4a27c02c273a7381
User <User@LAB3-3>
2023-09-07 12:38:16 +0300

    update sql queries

---
+++ DAL/SQL/Create dbo.StudentGrades.sql
@@ -0,0 +1,33 @@
+﻿USE [HighSchool]
+GO
+
+/****** Object: Table [dbo].[StudentGrades] Script Date: 07/09/2023 12:36:37 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[StudentGrades] (
+    [Id]        INT IDENTITY (1, 1) NOT NULL,
+    [Score]     INT NULL,
+    [StudentId] INT NOT NULL
+);
+
+
+USE [HighSchool]
+GO
+
+/****** Object: Table [dbo].[Students] Script Date: 07/09/2023 12:36:53 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Students] (
+    [Id]   INT        IDENTITY (1, 1) NOT NULL,
+    [Name] NCHAR (20) NOT NULL
+);
+
+
